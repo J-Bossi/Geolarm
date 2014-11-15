0d802bcf41d8a2adeb995cf97408af63405c94ce
Johannes Boß <joha.boss@gmail.com>
2014-11-15 19:40:24 +0100

    Buildable and runnable

--- app/src/main/java/de/jbossi/geolarm/MapsActivity.java
+++ app/src/main/java/de/jbossi/geolarm/MapsActivity.java
@@ -1,7 +1,7 @@
 package de.jbossi.geolarm;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
