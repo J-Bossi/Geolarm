7d50945de685c54b3d8341eb38af5a762cd4e48c
Johannes Boß <joha.boss@gmail.com>
2015-05-25 23:23:24 +0200

    Implemented Places Service

--- app/src/main/java/de/jbossi/geolarm/MapsActivity.java
+++ app/src/main/java/de/jbossi/geolarm/MapsActivity.java
@@ -1,23 +1,28 @@
 package de.jbossi.geolarm;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -25,6 +30,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Calendar;
 
 
 public class MapsActivity extends ActionBarActivity implements OnMapReadyCallback {
@@ -34,6 +41,8 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
     private Marker mAlarmMarker;
     private ImageButton mFloatingActionButton;
     private EditText editLocation;
+    private Place place;
+    int REQUEST_PLACE_PICKER = 1;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +60,7 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
                 showSetAlarmDialog();
             }
         });
-/*        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.SECOND, 5);
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-        AlarmManager am =
-                (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);*/
+
     }
 
 
@@ -96,12 +98,12 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
                 mAlarmMarker = mMap.getMap().addMarker(alarmMarkerOptions);
 
 
-                ;
+
             } else {
                 mAlarmMarker.setPosition(latLng);
 
             }
-            onPickButtonClick(getCurrentFocus());
+            onPickButtonClick(latLng);
         }
     };
 
@@ -109,32 +111,56 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
 
         MaterialDialog setUpDialog = new MaterialDialog.Builder(this).title("Alarm wählen!")
                 .customView(R.layout.set_alarm_dialog, true)
-                .positiveColor(R.color.primary_dark)
+                .positiveText(R.string.ok).negativeText(R.string.cancel)
+                .positiveColor(R.color.primary_dark).callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        Calendar cal = Calendar.getInstance();
+                        cal.add(Calendar.SECOND, 5);
+                        Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
+                        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),
+                                12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                        AlarmManager am = (AlarmManager) getApplicationContext().getSystemService(Activity.ALARM_SERVICE);
+                        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+                    }
+
+                    @Override
+                    public void onNegative(MaterialDialog dialog) {
+                    }
+                })
                 .build();
 
+
         editLocation = (EditText) setUpDialog.getCustomView().findViewById(R.id.locationText);
-        editLocation.setText(mAlarmMarker.getTitle());
+        if (place != null) {
+            editLocation.setText(place.getName());
+        }
         setUpDialog.show();
     }
 
-    public void onPickButtonClick(View v) {
-        // Construct an intent for the place picker
+    public void onPickButtonClick(LatLng latLng) {
+        if (true) {
+
         try {
-            PlacePicker.IntentBuilder intentBuilder =
-                    new PlacePicker.IntentBuilder();
+            PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+            intentBuilder.setLatLngBounds(Util.computeBounds(latLng));
             Intent intent = intentBuilder.build(this);
-            // Start the intent by requesting a result,
-            // identified by a request code.
+
             startActivityForResult(intent, REQUEST_PLACE_PICKER);
 
         } catch (GooglePlayServicesRepairableException e) {
-            // ...
+            GooglePlayServicesUtil
+                    .getErrorDialog(e.getConnectionStatusCode(), this, 0);
         } catch (GooglePlayServicesNotAvailableException e) {
-            // ...
+            Toast.makeText(this, "Google Play Services is not available.",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+
         }
     }
 
-    @Override
+
     protected void onActivityResult(int requestCode,
                                     int resultCode, Intent data) {
 
@@ -142,11 +168,8 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
                 && resultCode == Activity.RESULT_OK) {
 
             // The user has selected a place. Extract the name and address.
-            final Place place = PlacePicker.getPlace(data, this);
+            place = PlacePicker.getPlace(data, this);
 
-            final CharSequence name = place.getName();
-
-            editLocation.setText(name);
 
 
         } else {
@@ -160,6 +183,7 @@ public class MapsActivity extends ActionBarActivity implements OnMapReadyCallbac
 
 
     private LatLng getLastBestLocation() {
+
         Location locationGPS = mlocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         Location locationNet = mlocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
