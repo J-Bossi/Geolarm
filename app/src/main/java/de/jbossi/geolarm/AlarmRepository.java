e9f52accf86c5fbb76e8bab5f00b9df6611fb7aa
Johannes Boß <joha.boss@gmail.com>
2015-07-08 23:19:58 +0200

    Serialization finally works! Due to the use of Jackson and MixIns :relieved:

--- app/src/main/java/de/jbossi/geolarm/AlarmRepository.java
+++ app/src/main/java/de/jbossi/geolarm/AlarmRepository.java
@@ -1,27 +1,19 @@
 package de.jbossi.geolarm;
 
 import android.content.Context;
-import android.os.Environment;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+import com.google.android.gms.maps.model.LatLng;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -30,18 +22,18 @@ import java.util.List;
 
 
 public class AlarmRepository {
-    private List<Alarm> mAlarms;
     private static AlarmRepository mInstance = null;
-    private Context m_Context;
-    private static ObjectMapper m_ObjectMapper = new ObjectMapper();
-    private Gson gson = new Gson();
-    private String toSave = new String();
+    private List<Alarm> mAlarms;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private SharedPreferences pref;
 
     public AlarmRepository(Context ctx) {
-        mAlarms = new ArrayList<>();
-        m_Context = ctx;
-        //Todo Load from file
+        mAlarms = new ArrayList<>() ;
+       pref = PreferenceManager
+                .getDefaultSharedPreferences(ctx);
 
+        mapper.addMixIn(LatLng.class, LatLngMixIn.class);
         mAlarms = GetObjectsFromFile();
 
     }
@@ -61,43 +53,56 @@ public class AlarmRepository {
 
 
     public List<Alarm> GetObjectsFromFile() {
-        Type datasetListType = new TypeToken<Collection<Alarm>>() {}.getType();
 
-        if (!toSave.isEmpty()) {
-            mAlarms = gson.fromJson(toSave, datasetListType);
-            Log.i("Get", toSave);
+        String JSONObject = pref.getString("Alarms",new String());
+        if (!JSONObject.isEmpty()) {
+            Log.i("GET-JSON", JSONObject);
+            try {
+                mAlarms = mapper.readValue(JSONObject, new TypeReference<List<Alarm>>() {
+                });
+
+
+
+            } catch (JsonMappingException e) {
+                e.printStackTrace();
+            } catch (JsonParseException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-            return mAlarms;
-
-
-
-    }
-
-    public void SaveObjectsToFile(List<Alarm> alarms) {
-        Type datasetListType = new TypeToken<Collection<Alarm>>() {}.getType();
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-       toSave = gson.toJson(alarms,datasetListType );
-        Log.i("Save", toSave);
+        return mAlarms;
 
     }
 
 
 
 
+
+
+public void SaveObjectsToFile(List<Alarm> alarms) {
+
+        SharedPreferences.Editor editor = pref.edit();
+
+   StringWriter writer = new StringWriter();
+
+    try {
+        mapper.writeValue(writer, mAlarms);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    editor.putString("Alarms", writer.toString() );
+        editor.commit();
+    }
 
     public List<Alarm> getmAlarms() {
         return mAlarms;
     }
 
-    public void setmAlarms(List<Alarm> mAlarms) {
-        this.mAlarms = mAlarms;
-    }
-
     public void addAlarm(Alarm alarm) {
         mAlarms.add(alarm);
         SaveObjectsToFile(mAlarms);
-
     }
 
     public void removeAlarm(Alarm alarm) {
